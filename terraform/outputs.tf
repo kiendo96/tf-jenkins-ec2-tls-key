8a86dc0e321513d972e1a81b6472948a0baccb23
Kien Do <dotrungkien.tk@gmail.com>
2025-04-05 01:01:58 +0700

    update file main.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,6 +13,11 @@ output "instance_id" {
   value       = aws_instance.web_server.id
 }
 
+output "instance_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.web_server.public_ip
+}
+
 output "instance_public_dns" {
   description = "The public DNS name of the EC2 instance"
   value       = aws_instance.web_server.public_dns
